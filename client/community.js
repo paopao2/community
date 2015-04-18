349441caee633f0cb1814608a993a2b99990d351
Chang Hu <changhu@yahoo-inc.com>
2015-04-18 12:33:41 -0700

    add login by facebook

--- client/community.js
+++ client/community.js
@@ -21,3 +21,21 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Template.login.events({
+    'click #facebook-login': function(event) {
+        Meteor.loginWithFacebook({}, function(err){
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+
+    'click #logout': function(event) {
+        Meteor.logout(function(err){
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        });
+    }
+});
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1585410211738549',
+    secret: 'ad7f51677499c7a144d0dbe609d1a9f4'
+});
